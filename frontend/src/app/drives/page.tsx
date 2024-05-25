8e83b1e24f1c5480bec1528ff99f2a8d65743b81
s4nat <sanatkhandekar@gmail.com>
2024-05-26 04:08:16 +0800

    fixed drives

--- frontend/src/app/drives/page.tsx
+++ frontend/src/app/drives/page.tsx
@@ -1,20 +1,52 @@
+"use client";
 import DriveCard from "../components/DriveCard";
+import DriveProps from "../props/DriveProps";
 import styles from "../constants/style";
+import { useState, useEffect } from "react";
 
-export default async function Home() {
+export default function Home() {
+  const [drives, setDrives] = useState<DriveProps[]>([]);
+  useEffect(() => {
+    const fetchDrives = async () => {
+      try {
+        const response = await fetch("https://civic-kohl.vercel.app/drive");
+        const data: DriveProps[] = await response.json();
+        setDrives(data);
+      } catch (error) {
+        console.error("Error fetching Drives:", error);
+      }
+    };
+
+    fetchDrives();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col justify-center items-left p-24 z-0">
+    <main className="flex min-h-screen flex-col justify-center items-center p-24 z-0">
       <div className={styles.boxWidth}>
         <div className={`flex justify-start mt-2`}>
           <div
             className={`font-playfair  xs:text-[48px] text-[48px] text-[#1E1E1E] xs:leading-[76.8px] leading-[66.8px] w-full`}
           >
-            Current Drives
+            Our Drives
           </div>
         </div>
       </div>
       <div className="mt-5 grid grid-cols-1 sm:grid-cols-4 gap-[20px]">
-        <DriveCard name="Drive 1" description="A very impactful drive" month={12} amount={0} category="Fitness" />
+        {drives.map((drive, index) => {
+          return (
+            <div key={drive.drive_id}>
+              <DriveCard
+                id={drive.drive_id}
+                name={drive.drive_name}
+                description={drive.drive_description}
+                month={drive.drive_month}
+                amount={drive.drive_amount}
+                category={drive.drive_category}
+
+              />
+            </div>
+          );
+        })}
       </div>
     </main>
   );
---
+++ frontend/src/app/props/DriveProps.tsx
@@ -0,0 +1,8 @@
+export default interface DriveProps {
+    drive_id: string
+    drive_name: string;
+    drive_description: string;
+    drive_month: number;
+    drive_amount: number;
+    drive_category: string;
+}
