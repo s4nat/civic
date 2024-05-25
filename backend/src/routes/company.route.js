ba152f69191825416499d78bbe4598d3ceef99d0
rmurarishetti <64725000+rmurarishetti@users.noreply.github.com>
2024-05-26 03:23:05 +0800

    feat:Crash Recovery

--- backend/src/routes/company.route.js
+++ backend/src/routes/company.route.js
@@ -5,6 +5,6 @@ const companyController = require("../controllers/company.controller.js");
 router.get("/", companyController.getAllCompanies);
 router.post("/createCompany", companyController.createCompany);
 router.get("/fundAmountByCategory", companyController.getFundAmountByCategory);
-router.get("/sumCompanyFunds", companyController.getSumCompanyFunds);
+router.get("/sumCompanyFunds", companyController.sumCompanyFunds);
 
 module.exports = router;
