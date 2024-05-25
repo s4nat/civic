2538d90101b63a4a5c34b85060d5706cd1843bc7
s4nat <sanatkhandekar@gmail.com>
2024-05-26 02:16:26 +0800

    navbar fixes

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { close, logo2, menu } from "../../../public";
-import { navLinks } from "../constants";
+import { signedInLinks, signedOutLinks } from "../constants";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../constants/style";
@@ -19,27 +19,42 @@ const Navbar = () => {
       <div className={``}>
         <nav className={`${styles.paddingX} xl:max-w-full w-full fixed top-0 flex justify-between py-6 items-center navbar z-10 bg-[#262D44] px-6`}>
           <div className={`w-1/2 flex justify-start`}>
-            <Image src={logo2} alt="hoobank" className="w-[54px] h-[52px]" />
+            <Image src={logo2} alt="logo" className="w-[54px] h-[52px]" />
             <div className="font-playfair font-medium text-[24px] text-[#C2CBDC] hover:text-[#7C0000] ml-2 flex items-center">
               <a href="https://civic-kohl.vercel.app/">
-                CIVIC. 
+                CIVIC.
               </a>
             </div>
           </div>
           <ul className="list-none sm:flex hidden justify-end items-center flex-1 pr-1">
 
-            {navLinks.slice(0, user ? 1 : navLinks.length).map((nav, i) => (
+            {!user && signedOutLinks.map((nav, i) => (
               <li
                 key={nav.id}
-                className={`font-playfair font-normal cursor-pointer text-[16px] ${i === navLinks.length - 1 ? "mr-0" : "mr-10"
+                className={`font-playfair font-normal cursor-pointer text-[16px] ${i === signedOutLinks.length - 1 ? "mr-0" : "mr-10"
                   } text-[#C2CBDC] hover:text-lime-500 mr-10 ${currentRoute === `/#${nav.id}`
                     ? "border-b-4 border-white"
                     : "border-b-4 border-transparent"
                   }`}
               >
-                <a href={`/#${nav.id}`}>{nav.title}</a>
+                <Link href={nav.link}>{nav.title}</Link>
               </li>
             ))}
+
+            {user && signedInLinks.map((nav, i) => (
+              <li
+                key={nav.id}
+                className={`font-playfair font-normal cursor-pointer text-[16px] ${i === signedInLinks.length - 1 ? "mr-0" : "mr-10"
+                  } text-[#C2CBDC] hover:text-lime-500 mr-10 ${currentRoute === `/#${nav.id}`
+                    ? "border-b-4 border-white"
+                    : "border-b-4 border-transparent"
+                  }`}
+              >
+                <Link href={nav.link}>{nav.title}</Link>
+              </li>
+            ))}
+
+
             {user && (
               <li className="font-playfair font-normal cursor-pointer text-[16px] text-bold text-[#C2CBDC] mr-10">
                 <Link href={"/user/home"}>Dashboard</Link>
